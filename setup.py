741d1d631f0dad1ae388dbf7616f7928376b35c5
JeffreyLuo2023 <135291720+JeffreyLuo2023@users.noreply.github.com>
2023-06-02 10:07:55 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='gytest1',
+    version='1.0',
+    author='Jeffrey Luo',
+    author_email='1508598497@qq.com',
+    description='My Package',
+    packages= find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
